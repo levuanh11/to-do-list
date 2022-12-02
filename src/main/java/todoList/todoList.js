550e33956b35b2594c0e7cf6b46609b71be9168c
unknown <levuanh11>
2022-12-02 11:02:55 +0700

    second commit

--- src/main/java/todoList/todoList.js
+++ src/main/java/todoList/todoList.js
@@ -1,6 +1,7 @@
 document.querySelector('#push').onclick = function () {
     if (document.querySelector('#new-task input').value.length === 0) {
-        alert("please enter a task")
+        // alert("please enter a task")
+        failNotify();
     } else {
         document.querySelector('#tasks').innerHTML += `
         <div class="task">
@@ -10,12 +11,47 @@ document.querySelector('#push').onclick = function () {
             <button class="delete" > Delete</button>
           </div>
         `;
-
+        successNotify();
+        document.querySelector('#new-task input').value = "";
         let currentTask = document.querySelectorAll(".delete");
         for (let i = 0; i < currentTask.length; i++) {
             currentTask[i].onclick = function () {
                 this.parentNode.remove();
+                deleteNotify();
             }
         }
     }
+}
+
+
+let myNotify
+
+function failNotify() {
+    myNotify = new Notify({
+        status: 'error',
+        title: 'Notify Title',
+        text: 'please enter a task',
+        effect: 'slide',
+        type: 3
+    })
+}
+
+function successNotify() {
+    myNotify = new Notify({
+        status: 'success',
+        title: 'Notify Title',
+        text: 'added task successfully',
+        effect: 'slide',
+        type: 3
+    })
+}
+
+function deleteNotify() {
+    myNotify = new Notify({
+        status: 'success',
+        title: 'Notify Title',
+        text: 'delete task successfully',
+        effect: 'slide',
+        type: 3
+    })
 }
